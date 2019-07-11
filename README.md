5193f2d7fb7a68ae7ab5724a5b3516df89ac20c0
Victor Zhang <victorzw895@gmail.com>
2019-07-11 21:35:26 +1000

    eddited readme

--- README.md
+++ README.md
@@ -1,28 +1,41 @@
 # Project 0: Bomberman
 
-This is a simple game I built because Joel made me.
+Remake of bomberman, because its my all time favourite retro game.
+
+## Link - Deployed on GitHub Pages
+[Bomberman Game](https://victorzw895.github.io/project0-bomberman/)
 
 ## Features
+- Move with arrow keys
+- Place a bomb with space bar
+- Detect collision with outer walls, bricks, inner walls
+- Clear bricks and path with bombs, able to walk through
 
 
 ## Current Bugs
 
-1. burger: Burger bomb glitches when too many are placed at the same time.
+1. *burger*: Burger bomb glitches when too many are placed at the same time.
 
 ## Fixed Bugs
 
-1. burger: TEMPORARY FIX, reduced number of bombs to one at a time.
+1. *burger*: TEMPORARY FIX, reduced number of bombs to one at a time.
    bombs seem to have stopped glitching.
+2. *burger*: Would only not blow up brick bellow bomb, if there was a brick above the bomb.
 
 
 ## TODO
+- Add bots
+- Add AI to bots
+- allow bomb to destroy bots, bricks and player
+- make player and bots unable to pass through bombs
+- power ups (extra bombs, explode range, *push bombs*)
 - network multiplayer
 - more colours
 - more intense emotions
+- change board size
 
 ## Technologies
-- Made with jquery
-- Also uses funText to make the text fun
+- Made with jQuery
 
 ## Thank You
-- OMG Rashida YOU SAVED ME
+- Joel and Rashida for what you've taught me about HTML, CSS, JavaScript, DOM, jQuery so far!
--- js/bomberman.js
+++ js/bomberman.js
@@ -1,7 +1,7 @@
 console.log('bomberman is running!');
 
 
-
+// Board's length and width in squares
 let boardX = 19;
 let boardY = 11;
 
@@ -10,67 +10,93 @@ $(document).ready(function () {
 
   // initializeKeyboard();
 
+  // tracking mouse for testing purposes ///////////////////////////////////////
   $(window).mousemove(function(event) {
     console.log(event.pageX, event.pageY);
   })
+  //---------------------------------------------------------------------------
 
+
+  // FUTURE FUNCTION, create player and extra players on the board /////////////
   const createPlayer = function() {
 
   }
-
+  // Not sure if need this function atm ...
   const createBomb = function() {
 
   }
+  // --------------------------------------------------------------------------
 
+
+  // createBoard according to squares along boardX and boardY //////////////////
   const createBoard = function() {
+    // for every x in x axis
     for (let x = 0; x < boardX; x++) {
+      // for every y in y axis, create a column for every x from outer loop
       for (let y = 0; y < boardY; y++) {
+        // first column, last row, last column, first row -> make border
         if (x === 0 || y === boardY-1 || x === boardX-1 || y === 0) {
           $('.wrapper').append("<div class='border impenetrable'></div>");
         }
+        // for every even box after border, (border starts with 0)
         else if (x%2 === 0 && y%2 === 0) {
           $('.wrapper').append("<div class='walls impenetrable'></div>");
         }
+        // for every column between 4th and 16th
         else if (x >= 3 && x <= boardX-4) {
+          // compulsory bricks blocking player starting position
           if (x === 3 && y === 1) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // compulsory bricks blocking player starting position
           else if (x === boardX-4 && y === 1) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // compulsory bricks blocking player starting position
           else if (x === 3 && y === boardY-2) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // compulsory bricks blocking player starting position
           else if (x === boardX-4 && y === boardY-2) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // boolean, if number over 0.3/1, make brick (random generate bricks)
           else if (Math.random() >= 0.3) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // the ones that did not become bricks, make regular box
           else {
             $('.wrapper').append("<div class='box'></div>");
           }
         }
+        // for remaining spaces, every row between, 4th and 8th
         else if (y >= 3 && y <= boardY-4) {
+          // compulsory bricks blocking player starting position
           if (x === 1 && y === 3) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // compulsory bricks blocking player starting position
           else if (x === boardX-2 && y === 3) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // compulsory bricks blocking player starting position
           else if (x === 1 && y === boardY-4) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // compulsory bricks blocking player starting position
           else if (x === boardX-2 && y === boardY-4) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // boolean, if number over 0.3/1, make brick (random generate bricks)
           else if (Math.random() >= 0.3) {
             $('.wrapper').append("<div class='brick impenetrable'></div>");
           }
+          // the ones that did not become bricks, make regular box
           else {
             $('.wrapper').append("<div class='box'></div>");
           }
         }
+        // 3 boxes each corner for player starting position
         else {
           $('.wrapper').append("<div class='box'></div>");
         }
@@ -80,6 +106,5 @@ $(document).ready(function () {
 
 
   createBoard();
-
 
 });
