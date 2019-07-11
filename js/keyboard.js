672b26f160271b11fe9573b31ba62c1188508fe2
Victor Zhang <victorzw895@gmail.com>
2019-07-11 10:32:57 +1000

    working collision with player

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,8 +1,83 @@
 console.log('bomberman is running!');
 
-$(document).ready(function() {
 
+canMove = {
+  up: true,
+  right: true,
+  down: true,
+  left: true
+}
+
+
+const checkImpenetrable = function() {
+  let $impenetrable = $('.impenetrable');
+
+  let topX = $('.top').position().left;
+  let topY = $('.top').position().top;
+  let rightX = $('.right').position().left;
+  let rightY = $('.right').position().top;
+  let bottomX = $('.bottom').position().left;
+  let bottomY = $('.bottom').position().top;
+  let leftX = $('.left').position().left;
+  let leftY = $('.left').position().top;
+
+  canMove.up = true;
+  canMove.right = true;
+  canMove.down = true;
+  canMove.left = true;
+
+
+  for (let i = 0; i < $impenetrable.length; i ++) {
+
+    if (topX === obstacles['obstacle'+i].left && topY === obstacles['obstacle'+i].top) {
+      console.log('cant go up!');
+      canMove.up = false;
+    }
+    else if ((topX === ((obstacles['obstacle'+i].left) + 26) && topY === obstacles['obstacle'+i].top) ||
+             (topX === ((obstacles['obstacle'+i].left) - 26) && topY === obstacles['obstacle'+i].top)) {
+      console.log('still cant go up!');
+      canMove.up = false;
+    }
+
+    if (rightX === obstacles['obstacle'+i].left && rightY === obstacles['obstacle'+i].top) {
+      console.log('cant go right');
+      canMove.right = false;
+    }
+    else if ((rightX === obstacles['obstacle'+i].left && rightY === (obstacles['obstacle'+i].top) + 26) ||
+             (rightX === obstacles['obstacle'+i].left && rightY === (obstacles['obstacle'+i].top) - 26)) {
+      console.log('still cant go right!');
+      canMove.right = false;
+    }
+
+    if (bottomX === obstacles['obstacle'+i].left && bottomY === obstacles['obstacle'+i].top) {
+      console.log('cant go down!');
+      canMove.down = false;
+    }
+    else if ((bottomX === ((obstacles['obstacle'+i].left) + 26) && bottomY === obstacles['obstacle'+i].top) ||
+             (bottomX === ((obstacles['obstacle'+i].left) - 26) && bottomY === obstacles['obstacle'+i].top)) {
+      console.log('still cant go down!');
+      canMove.down = false;
+    }
+
+    if (leftX === obstacles['obstacle'+i].left && leftY === obstacles['obstacle'+i].top) {
+      console.log('cant go left!');
+      canMove.left = false;
+    }
+    else if ((leftX === obstacles['obstacle'+i].left && leftY === (obstacles['obstacle'+i].top) + 26) ||
+             (leftX === obstacles['obstacle'+i].left && leftY === (obstacles['obstacle'+i].top) - 26)) {
+      console.log('still cant go left!');
+      canMove.left = false;
+    }
+
+  }
+  console.log(canMove);
+}
+
+$(document).ready(function() {
+  checkImpenetrable();
 })
+
+
   const pressedKeys = {};
   let moving = false;
 
@@ -10,6 +85,7 @@ $(document).ready(function() {
     // event.preventDefault();
     // if (e.which === 37 || e.which === 38 || e.which === 39 || e.which === 40 ) {
           pressedKeys[e.which] = true;
+          // checkImpenetrable();
     // }
     // console.log(pressedKeys)
   })
@@ -17,6 +93,7 @@ $(document).ready(function() {
   $(document).keyup(function(e) {
     // event.preventDefault();
     delete pressedKeys[e.which];
+    // checkImpenetrable();
   })
 
 
@@ -62,7 +139,6 @@ $(document).on("keydown",(function(e) {
   // checkMoves();
   // NEED TO UPDATE BOMBERMAN LOCATION
 
-  let $impenetrable = $('.impenetrable');
 
   // if (bX < (obstacles['obstacle'+i].left + 50) &&
   //     bX + 50 > obstacles['obstacle'+i].left &&
@@ -100,7 +176,7 @@ $(document).on("keydown",(function(e) {
 
 
 
-  if (e.which === 37 || e.which === 38 || e.which === 39 || e.which === 40 ) {
+  if ((e.which === 37 && canMove.left) || (e.which === 38 && canMove.up) || (e.which === 39 && canMove.right) || (e.which === 40 && canMove.down) ) { // dont run for any other button except arrow keys
     // console.log(bomberman);
     // check hit impenetrable
 
@@ -113,16 +189,6 @@ $(document).on("keydown",(function(e) {
 
     // else {
       const move = function() {
-        let bX = $('#bomberman').position().left;
-        let bY = $('#bomberman').position().top;
-        let topX = $('.top').position().left;
-        let topY = $('.top').position().top;
-        let rightX = $('.right').position().left;
-        let rightY = $('.right').position().top;
-        let bottomX = $('.bottom').position().left;
-        let bottomY = $('.bottom').position().top;
-        let leftX = $('.left').position().left;
-        let leftY = $('.left').position().top;
         // console.log(bX);
         // console.log(bY);
         $('#bomberman').css(directionSpeed[e.which])
@@ -142,7 +208,10 @@ $(document).on("keydown",(function(e) {
         // else if (e.which === 40) {
         //   bomberman.bY += 26;
         // }
-        moving = true;
+
+
+        moving = true; // move code runs only once, maybe remove this
+
 
         // if (bX < 200) {
         //   console.log('this should work')
@@ -201,56 +270,7 @@ $(document).on("keyup",(function(e) {
     // clearInterval(intervalId)
     moving = false;
   }
-    let $impenetrable = $('.impenetrable');
 
-  let bX = $('#bomberman').position().left;
-  let bY = $('#bomberman').position().top;
-  let topX = $('.top').position().left;
-  let topY = $('.top').position().top;
-  let rightX = $('.right').position().left;
-  let rightY = $('.right').position().top;
-  let bottomX = $('.bottom').position().left;
-  let bottomY = $('.bottom').position().top;
-  let leftX = $('.left').position().left;
-  let leftY = $('.left').position().top;
-
-
-  for (let i = 0; i < $impenetrable.length; i ++) {
-    if (topX === obstacles['obstacle'+i].left && topY === obstacles['obstacle'+i].top) {
-      console.log('cant go top!');
-    }
-    else if ((topX === ((obstacles['obstacle'+i].left) + 26) && topY === obstacles['obstacle'+i].top) ||
-             (topX === ((obstacles['obstacle'+i].left) - 26) && topY === obstacles['obstacle'+i].top)) {
-      console.log('still cant go top!');
-    }
-
-
-    if (rightX === obstacles['obstacle'+i].left && rightY === obstacles['obstacle'+i].top) {
-      console.log('cant go right');
-    }
-    else if ((rightX === obstacles['obstacle'+i].left && rightY === (obstacles['obstacle'+i].top) + 26) ||
-             (rightX === obstacles['obstacle'+i].left && rightY === (obstacles['obstacle'+i].top) - 26)) {
-      console.log('still cant go right!');
-    }
-
-    if (bottomX === obstacles['obstacle'+i].left && bottomY === obstacles['obstacle'+i].top) {
-      console.log('cant go down!');
-    }
-    else if ((bottomX === ((obstacles['obstacle'+i].left) + 26) && bottomY === obstacles['obstacle'+i].top) ||
-             (bottomX === ((obstacles['obstacle'+i].left) - 26) && bottomY === obstacles['obstacle'+i].top)) {
-      console.log('still cant go down!');
-    }
-
-    if (leftX === obstacles['obstacle'+i].left && leftY === obstacles['obstacle'+i].top) {
-      console.log('cant go left!');
-    }
-    else if ((leftX === obstacles['obstacle'+i].left && leftY === (obstacles['obstacle'+i].top) + 26) ||
-             (leftX === obstacles['obstacle'+i].left && leftY === (obstacles['obstacle'+i].top) - 26)) {
-      console.log('still cant go left!');
-    }
-
-
-}
-
+  checkImpenetrable();
 
 }))
